6e73428a5b7c5ad2b2f7b32fed85c4b28a711bf3
João Victor C. Domingues <joao.domingues@b2wdigital.com>
2021-08-27 16:30:53 -0300

    Adicionando icone e corrigindo tamanho do texto no header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 
 import { Box, Heading, Text } from '@chakra-ui/react';
 
+import { SiBitcoin } from 'react-icons/si';
+
 export default function Header() {
   return (
     <Box bg="gray.700" w="100%" p={4} d="flex" justifyContent="space-between" alignItems="center">
-      <Heading color="#fff" size="lg">Crypto Tracker</Heading>
+      <Box d="flex" alignItems="center">
+        <SiBitcoin color="white" size="2em" />
+        <Heading color="#fff" size="md" ml="4">Crypto Tracker</Heading>
+      </Box>
       <Text color="gray.500">&copy; jvcdomingues 2021</Text>
     </Box>
   )
