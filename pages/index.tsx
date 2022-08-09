dc2163ec708a7766bfbe240f179c583b98536ee4
João Victor <jvcd1999@hotmail.com>
2022-08-09 14:46:16 -0300

    Corrigindo pesquisa das moedas pelo input

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import { Grid, Box, Heading, Image, Text, Button, Input, FormControl, FormHelperText } from '@chakra-ui/react';
+import { Grid, Box, Heading, Image, Text, Button, Input, FormControl } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 import api from '../services/api';
 
@@ -23,29 +23,15 @@ type Coins = {
 
 const Home: NextPage<HomeProps> = ({ coins }) => {
   const [coinInput, setCoinInput] = useState('');
-  const [hasError, setHasError] = useState(false);
-  const [foundCoin, setFoundCoin] = useState({} as Coins);
   const router = useRouter();
 
   const handleNavigation = (uuid: string) => {
     router.push(`/coin/${uuid}`);
   }
 
-  const handleSearchButton = () => {
-    const coin = coinInput && coins.find(coin => coin.name === coinInput);
-    if(coin) {
-      setFoundCoin(coin);
-      return;
-    }
-    setHasError(true);
-  }
+  const filteredCoins = coinInput.length > 0 ? coins.filter(coin => coin.name.toLowerCase().includes(coinInput)) : coins;
 
-  useEffect(() => {
-    if(!coinInput) {
-      setFoundCoin({} as Coins);
-      setHasError(false);
-    }
-  }, [coinInput]);
+  console.log(coinInput)
 
   return (
     <>
@@ -55,36 +41,37 @@ const Home: NextPage<HomeProps> = ({ coins }) => {
       <Box d="flex" alignItems="center">
         <FormControl id="coin">
           <Input 
-            isInvalid={hasError}
             placeholder="Pesquise uma moeda"
             w="250px"
             color="white"
             mr="5"
             onChange={event => setCoinInput(event.target.value)}
           />
-          {hasError && <FormHelperText>Moeda não encontrada</FormHelperText>}
-          <Button colorScheme="teal" onClick={handleSearchButton}>Pesquisar</Button>
         </FormControl>
       </Box>
-      {Object.keys(foundCoin).length !== 0 && coinInput ? (
-        <Box 
-          bg="gray.700"
-          key={foundCoin.uuid} 
-          borderRadius="md" 
-          p="4" 
-          w="350px"
-          boxShadow="0px 0px 2px 2px rgba(0,0,0,0.1)"
-          mt="5"
-        >
-          <Box d="flex" alignItems="center">
-            <Image src={foundCoin.iconUrl} size="sm" alt={foundCoin.name} boxSize="40px" />
-            <Heading size="md" color="#fff" ml="5">{foundCoin.name}</Heading>
-          </Box>
+      {coinInput ? (
+        <Grid templateColumns="repeat(3, 1fr)" gap="10" w="100%" mt="5">
+        {filteredCoins.map(coin => (
+            <Box 
+              bg="gray.700"
+              borderRadius="md" 
+              p="4" 
+              w="100%"
+              boxShadow="0px 0px 2px 2px rgba(0,0,0,0.1)"
+              mt="5"
+              key={coin.uuid}
+            >
+              <Box d="flex" alignItems="center">
+                <Image src={coin.iconUrl} size="sm" alt={coin.name} boxSize="40px" />
+                <Heading size="md" color="#fff" ml="5">{coin.name}</Heading>
+              </Box>
 
-          <Text mt="5" color="white" fontWeight="bold">$ {foundCoin.price}</Text>
+              <Text mt="5" color="white" fontWeight="bold">$ {coin.price}</Text>
 
-          <Button w="100%" mt="5" colorScheme="telegram" onClick={() => handleNavigation(foundCoin.uuid)}>Ver Detalhes</Button>
-        </Box>
+              <Button w="100%" mt="5" colorScheme="telegram" onClick={() => handleNavigation(coin.uuid)}>Ver Detalhes</Button>
+            </Box>
+        ))}
+        </Grid> 
       ) : (
       <Grid templateColumns="repeat(3, 1fr)" gap="10" w="100%" mt="5">
         {coins.map(coin => (
@@ -119,7 +106,7 @@ export async function getStaticProps() {
   const { data } = responseData;
   const { coins } = data;
 
-  const filteredData = coins.map((coin: { uuid: string; name: string; symbol: string; color: string; iconUrl: string; change: string; price: string; }) => ({
+  const filteredData = coins.map((coin: Coins) => ({
     uuid: coin.uuid,
     name: coin.name,
     symbol: coin.symbol,
